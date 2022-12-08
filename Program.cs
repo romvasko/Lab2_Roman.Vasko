1b99b66f76a05fa3b13c498f6d5c5d53fb73a1c7
romvasko <romvasko@yandex.by>
2022-12-08 16:03:23 +0300

    final

--- Program.cs
+++ Program.cs
@@ -9,42 +9,42 @@ namespace Lab2_Roman.Vasko // Note: actual namespace depends on the project name
     internal class Program {
         static void Main(string[] args) {
 
-            //Console.WriteLine(Lab2String.Task0(args[0]));
-            //Console.WriteLine(Lab2String.Task1(args[0]));
-            //Console.WriteLine(Lab2String.Task2(args[0]));
-            //Console.WriteLine(Lab2String.Task3(args[0]));
-            //Console.WriteLine(Lab2String.Task4(args[0]));
-            //Console.WriteLine(Lab2String.Task5(args[0]));
-            //Console.WriteLine(Lab2String.Task6(args[0]));
+            Console.WriteLine(Lab2String.Task0(args[0]));
+            Console.WriteLine(Lab2String.Task1(args[0]));
+            Console.WriteLine(Lab2String.Task2(args[0]));
+            Console.WriteLine(Lab2String.Task3(args[0]));
+            Console.WriteLine(Lab2String.Task4(args[0]));
+            Console.WriteLine(Lab2String.Task5(args[0]));
+            Console.WriteLine(Lab2String.Task6(args[0]));
 
-            //Lab2Array.First();
-            //Lab2Array.Second();
-            //Lab2Array.Third();
-            //Lab2Array.Fourth();
-            //Lab2Array.Fifth();
-            //Lab2Array.Sixth();
-            //Lab2Array.Seventh();
-            //Lab2Array.Eighth();
-            //Lab2Array.Nineth();
+            Lab2Array.First();
+            Lab2Array.Second();
+            Lab2Array.Third();
+            Lab2Array.Fourth();
+            Lab2Array.Fifth();
+            Lab2Array.Sixth();
+            Lab2Array.Seventh();
+            Lab2Array.Eighth();
+            Lab2Array.Nineth();
 
-            string funk = Console.ReadLine().Replace(" ","");
-            Console.WriteLine(funk);
-            char[] splitChar = {'&','|','^' };
-            if(funk.IndexOf("&") != -1) {
-            string[] args1 = funk.Split("&");
-                string token = "&";
-                Lab2Hard.Table(args1, token, funk);
-            }
-            if (funk.IndexOf("|") != -1) {
-                string[] args1 = funk.Split("|");
-                string token = "|";
-                Lab2Hard.Table(args1, token, funk);
-            }
-            if (funk.IndexOf("^") != -1) {
-                string[] args1 = funk.Split("^");
-                string token = "^";
-                Lab2Hard.Table(args1, token, funk);
-            }
+            //string funk = Console.ReadLine().Replace(" ","");
+            //Console.WriteLine(funk);
+            //char[] splitChar = {'&','|','^' };
+            //if(funk.IndexOf("&") != -1) {
+            //string[] args1 = funk.Split("&");
+            //    string token = "&";
+            //    Lab2Hard.Table(args1, token, funk);
+            //}
+            //if (funk.IndexOf("|") != -1) {
+            //    string[] args1 = funk.Split("|");
+            //    string token = "|";
+            //    Lab2Hard.Table(args1, token, funk);
+            //}
+            //if (funk.IndexOf("^") != -1) {
+            //    string[] args1 = funk.Split("^");
+            //    string token = "^";
+            //    Lab2Hard.Table(args1, token, funk);
+            //}
         }
 
         
